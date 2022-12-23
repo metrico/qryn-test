e6bc33a8706e0aaabb00d9bb04f8a291a0f535b6
akvlad <akvlad90@gmail.com>
2022-12-23 16:19:54 +0200

    misc tests

--- e2e.misc.js
+++ e2e.misc.js
@@ -28,3 +28,10 @@ _it('should get /api/v1/status/buildinfo', async () => {
     expect((await axiosGet(`http://${clokiExtUrl}/api/v1/status/buildinfo`)).status).toEqual(200)
 })
 
+_it('should get /influx/api/v2/write/health', async () => {
+    expect((await axiosGet(`http://${clokiExtUrl}/influx/api/v2/write/health`)).status).toEqual(200)
+})
+
+_it('should get /influx/health', async () => {
+    expect((await axiosGet(`http://${clokiExtUrl}/influx/health`)).status).toEqual(200)
+})
