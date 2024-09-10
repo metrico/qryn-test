51560d9188cfc616d34ed7fb70d2faf27fb47ad3
akvlad <akvlad90@gmail.com>
2024-09-10 10:29:34 +0300

    clickhouse cluster tests env

---
+++ compose/clickhouse_cluster_e2e/run_test.sh
@@ -0,0 +1,14 @@
+docker compose -f test/e2e/compose/clickhouse_cluster_e2e/docker-compose.yaml up -d && \
+sleep 5 && \
+docker run -i --rm \
+  --network clickhouse_cluster_e2e_qryn-e2e \
+  -v `pwd`:/qryn \
+  -e 'INTEGRATION_E2E=1' \
+  -e 'CLOKI_EXT_URL=qryn:3100' \
+  -e 'QRYN_LOGIN=a' \
+  -e 'QRYN_PASSWORD=b' \
+  -e 'OTEL_COLLECTOR_URL=http://otel-collector:8062' \
+  node:${NODE_VERSION} sh -c 'cd /qryn && npm install && npm run test';
+code=$?;
+docker compose -f test/e2e/compose/clickhouse_cluster_e2e/docker-compose.yaml down;
+exit $code
